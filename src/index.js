2022d0af83024aa575ee996ab55ea0ca5490c6ec
Stephanie Marie <76826555+MutableSpectre@users.noreply.github.com>
2024-08-19 15:59:29 -0400

    Add API Functionality in JavaScript
    Added the API and JavaScript to create a workable search engine for weather and city. Also, added id's to html.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+function updateWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+     let apiKey = "40e28c77t4bb5368od54b9ccc0f6fad9";
+     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+     axios.get(apiUrl).then(updateWeather);
+    }
+
 function searchSubmit(event) {
     event.preventDefault();
     let searchInputElement = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInputElement.value;
+    searchCity(searchInputElement.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchSubmit);
+
+searchCity("Lansing");
