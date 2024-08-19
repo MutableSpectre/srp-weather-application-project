fbcf408beb3e516ddd262195bb6b902badb84bb5
Stephanie Marie <76826555+MutableSpectre@users.noreply.github.com>
2024-08-19 16:45:10 -0400

    Added JavaScript and HTML
    JavaScript: added description, humidity, wind speed, and date/time functionality.
    
    HTML: added span and id to humidity and wind. Deleted text 'Friday 6:55, broken clouds', '67%', and '4.63km/h.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ function updateWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let windSpeed = response.data.wind.speed;
 
     cityElement.innerHTML = response.data.city;
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${Math.round(windSpeed)} mph`;
     temperatureElement.innerHTML = Math.round(temperature);
 }
 
@@ -19,7 +26,39 @@ function searchSubmit(event) {
     searchCity(searchInputElement.value);
 }
 
+function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let day = date.getDay();
+  
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+  
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+  
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ];
+  
+    let formattedDay = days[day];
+    return `${formattedDay} ${hours}:${minutes}`;
+  }
+
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchSubmit);
 
 searchCity("Lansing");
+
+let currentDateElement = document.querySelector("#current-date");
+let currentDate = new Date();
+
+currentDateElement.innerHTML = formatDate(currentDate);
