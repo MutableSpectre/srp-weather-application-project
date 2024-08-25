b384def64ae1a147c0b5a6038c26e259e02830b2
Stephanie Marie <76826555+MutableSpectre@users.noreply.github.com>
2024-08-25 01:58:51 -0400

    Changes to CSS and JS
    Changed CSS to make format look better.
    
    Integrated the API in JS to retrieve forecast data.

--- src/index.js
+++ src/index.js
@@ -63,25 +63,34 @@ function formatDate(date) {
     axios.get(apiUrl).then(displayForecast);
   }
 
+  function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[date.getDay()];
+  }
+
   function displayForecast(response) {
     let days = ["Sun", "Mon", "Tue", "Wed", "Thu"];
     let forecastHtml = "";
 
-    days.forEach(function (day) {
+    response.data.daily.forEach(function (day, index) {
+      if (index < 5) {
       forecastHtml = 
         forecastHtml + 
         `
         <div class="weather-forecast-day">
-          <div class="weather-forecast-date">${day}</div>
-          <div class="weather-forecast-icon">⛅</div>
+          <div class="weather-forecast-date">${formatDay(day.time)}</div>
+          <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
           <div class="weather-forecast-temperatures">
             <div class="weather-forecast-temperature">
-              <strong>15°</strong>
+              <strong>${Math.round(day.temperature.maximum)}°</strong>
             </div>
-            <div class="weather-forecast-temperature">9°</div>
+            <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}°</div>
           </div>
         </div>
       `;
+      }
     }); 
 
     let forecastElement = document.querySelector("#forecast");
